ecf0c855d2d75732724001649e6c653071ea0ba0
Faizaan Gagan <fzngagan@gmail.com>
2020-10-19 11:14:01 +0000

    FEATURE: Added a feed setting to fetch entries on or after a given date (#1)
    * Added a feed setting to fetch entries on or after a given date
    
    * ran rubocop
    
    * Added topic custom fields to use the topic form's url field
    
    * Fixed hbs formatting

--- app/jobs/jobs/discourse_rss_polling/poll_feed.rb
+++ app/jobs/jobs/discourse_rss_polling/poll_feed.rb
@@ -10,7 +10,7 @@ module Jobs
 
         @feed_url = args[:feed_url]
         @author = User.find_by_username(args[:author_username])
-
+        @start_date = args[:start_date]
         poll_feed if not_polled_recently?
       end
 
@@ -28,20 +28,30 @@ module Jobs
 
       def poll_feed
         topics_polled_from_feed[0].each do |topic|
+          next if (@start_date.present? && Date.parse(@start_date) > topic.created_at)
           raw = "#{topic.url}
 
           "
 
           category = EmbeddableHost.record_for_url(topic.url).category_id
+
           params = {
             raw: raw,
             category: category,
             archetype: 'regular',
-            title: "#{topic.title() +' - '+ topics_polled_from_feed[1]}",
+            title: "#{topic.title() + ' - ' + topics_polled_from_feed[1]}",
             topic_id: nil,
             featured_link: topic.url,
-            created_at: topic.created_at
+            created_at: topic.created_at,
+            topic_opts: {
+              custom_fields: {
+                new_topic_form_data: {
+                  url: topic.url
+                }
+              }
+            }
           }
+
           pm = NewPostManager.new(@author, params)
           pm.perform
         end
@@ -51,7 +61,7 @@ module Jobs
         raw_feed = fetch_raw_feed
         return [] if raw_feed.blank?
         parsed_feed = RSS::Parser.parse(raw_feed)
-        return parsed_feed.items.map { |item| ::DiscourseRssPolling::FeedItem.new(item) } , parsed_feed.channel.title
+        [parsed_feed.items.map { |item| ::DiscourseRssPolling::FeedItem.new(item) } , parsed_feed.channel.title]
       rescue RSS::NotWellFormedError, RSS::InvalidRSSError
         []
       end
